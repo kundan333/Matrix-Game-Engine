6aa2465a5da956b60450b064ebc6c6ce3cd58178
kundan <kundanbora77@gmail.com>
2018-03-28 01:26:44 +0530

    added projection and view matrix

---
+++ src/entities/Camera.java
@@ -0,0 +1,48 @@
+package entities;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector3f;
+
+public class Camera {
+	
+	private Vector3f position =new Vector3f(0,0,0);
+	private float pitch; //rotation in x,y,z
+	private float yaw; //Camera aim left right
+	private float roll; // roll 180 up side down
+	
+	
+	public void move() {
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			position.z -= 0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			position.x += 0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			position.x -= 0.02f;
+		}
+		
+	}
+	
+	
+	public Vector3f getPosition() {
+		return position;
+	}
+	public float getPitch() {
+		return pitch;
+	}
+	public float getYaw() {
+		return yaw;
+	}
+	public float getRoll() {
+		return roll;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -2,12 +2,17 @@ package shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
 
+import entities.Camera;
+import toolbox.Maths;
+
 public class StaticShader extends ShaderProgram {
 	
 
 	private static final String VERTEX_FILE ="src/shaders/vertexShader.txt";
 	private static final String FRAGMENT_FILE ="src/shaders/fragmentShader.txt";
 	private int location_transformationMatrix;
+	private int location_projectionMatrix;
+	private int location_viewMatrix;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -27,11 +32,23 @@ public class StaticShader extends ShaderProgram {
 		
 		// TODO Auto-generated method stub
 		location_transformationMatrix = super.getUnitformLocation("transformationMatrix");
+		location_projectionMatrix = super.getUnitformLocation("projectionMatrix");
+		location_viewMatrix = super.getUnitformLocation("viewMatrix");
+
 	}
 	
 	public void loadTransfromationMatrix(Matrix4f matrix) {
 		super.loadMatrix(location_transformationMatrix, matrix);
 	}
+	
+	public void loadProjectionMatrix(Matrix4f projection) {
+		super.loadMatrix(location_projectionMatrix, projection);
+	}
+	public void loadViewMatrix(Camera camera){
+		Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+		super.loadMatrix(location_viewMatrix, viewMatrix);
+	}
+	
 	
 	
 
